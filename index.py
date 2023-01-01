2cddb0d3d41c611964aeb90aa234a7ab8a816b66
sugarcane0905 <shelly910905@gmail.com>
2023-01-02 06:34:00 +0800

    make it better

--- index.py
+++ index.py
@@ -7,7 +7,7 @@ cred = credentials.Certificate("serviceAccountKey.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, make_response, jsonify
 app = Flask(__name__)
 
 @app.route("/")
@@ -57,7 +57,51 @@ def input():
                 info += "電話：" + doc.to_dict()["tel"] + "<br><br>"
             return info
         else:
-            return render_template("input.html")          
+            return render_template("input.html")
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    # build a request object
+    req = request.get_json(force=True)
+    # fetch queryResult from json
+    action =  req.get("queryResult").get("action")
+    #msg =  req.get("queryResult").get("queryText")
+    #info = "動作：" + action + "； 查詢內容：" + msg
+    if (action == "rateChoice"):
+        rate =  req.get("queryResult").get("parameters").get("rate")
+        info = "您選擇的縣市是：" + rate + "的電影院：\n"
+
+        collection_ref = db.collection("全台電影院")
+        docs = collection_ref.get()
+        result = ""
+        for doc in docs:
+            dict = doc.to_dict()
+            if rate in dict["rate"]:
+                result += "電影院名稱：" + dict["title"] + "\n"
+                result += "該電影院上映電影連結：" + dict["hyperlink"] + "\n"
+                result += "地址：" + dict["adds"] + "\n"
+                result += "電話：" + dict["tel"] + "\n\n"
+        info += result
+    elif (action == "MovieDetail"): 
+        cond =  req.get("queryResult").get("parameters").get("FilmQ")
+        keyword =  req.get("queryResult").get("parameters").get("any")
+        info = "您要查詢電影院的" + cond + "，關鍵字是：" + keyword + "\n\n"
+        if (cond == "電影院名稱"):
+            collection_ref = db.collection("全台電影院")
+            docs = collection_ref.get()
+            found = False
+            for doc in docs:
+                dict = doc.to_dict()
+                if keyword in dict["title"]:
+                    found = True 
+                    info += "電影院名稱：" + dict["title"] + "\n"
+                    info += "該電影院上映電影連結：" + dict["hyperlink"] + "\n"
+                    info += "地址：" + dict["adds"] + "\n" 
+                    info += "電話：" + dict["tel"] + "\n\n"
+            if not found:
+                info += "很抱歉，目前無符合這個關鍵字的相關電影院喔"
+    
+    return make_response(jsonify({"fulfillmentText": info}))
 
 if __name__ == "__main__":
     app.run()
